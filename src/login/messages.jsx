0130b284b5582ca575c265551ec3d52688d80c87
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2024-07-01 18:47:40 +0530

    reverted text

--- src/login/messages.jsx
+++ src/login/messages.jsx
@@ -9,7 +9,7 @@ const messages = defineMessages({
   // Login labels
   'login.user.identity.label': {
     id: 'login.user.identity.label',
-    defaultMessage: 'Email',
+    defaultMessage: 'Username or email',
     description: 'Label for user identity field to enter either username or email to login',
   },
   'login.password.label': {
