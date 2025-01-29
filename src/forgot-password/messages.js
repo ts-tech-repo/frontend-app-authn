caf98e260bc3c389b43b6f9667e0aa3d69a8725c
sohailm100 <158144414+sohailm100@users.noreply.github.com>
2025-01-29 15:06:08 +0530

    updated text changes for inactive users

--- src/forgot-password/messages.js
+++ src/forgot-password/messages.js
@@ -121,7 +121,7 @@ const messages = defineMessages({
   // Error messages
   'internal.server.error': {
     id: 'internal.server.error',
-    defaultMessage: 'An error has occurred. Try refreshing the page, or check your internet connection.',
+    defaultMessage: 'Your account has been deactivate. Please contact your support for more information.',
     description: 'Error message that appears when server responds with 500 error code',
   },
 });
