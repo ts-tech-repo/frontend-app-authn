aae67be85fb2843be514f8044a807967275a6a0a
manoj.p3 <manoj.p@talentsprint.com>
2025-02-03 18:06:08 +0530

    debug

--- src/forgot-password/ForgotPasswordAlert.jsx
+++ src/forgot-password/ForgotPasswordAlert.jsx
@@ -46,8 +46,7 @@ const ForgotPasswordAlert = (props) => {
      break;
     case INTERNAL_SERVER_ERROR:
       const inactive_msg = getConfig().INACTIVE_USER_MESSAGE;
-      console.log(inactive_msg);
-      message = "from here";
+      message = inactive_msg;
       break;
     case FORBIDDEN_STATE:
       heading = formatMessage(messages['forgot.password.error.message.title']);
